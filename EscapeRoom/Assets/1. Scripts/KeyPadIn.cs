84157404c7ce1d4ba0105dd908dbd5ace0d17564
artsung410 <zpzpskm@naver.com>
2022-08-26 04:03:06 +0900

    [Feat] 키패드 기능 추가 (1/2)

---
+++ EscapeRoom/Assets/1. Scripts/KeyPadIn.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyPadIn : MonoBehaviour
+{
+    public int Id;
+    //public static event Action<string, int> Rotated = delegate { };
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
